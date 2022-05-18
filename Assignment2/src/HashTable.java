83ebca9c4377d590eee24782e6fb55dbdac92135
tavtejsingh <tavtejsingh@tavtejs-air.wireless.concordia.ca>
2022-05-18 16:43:58 -0400

    initial setup test

--- Assignment2/src/HashTable.java
+++ Assignment2/src/HashTable.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class HashTable {
+	
+	private static class Node{
+		String key;
+		String value;
+		Node node;
+	}
+	
+	private Node[] node;
+	
+	private int count;
 
 }
